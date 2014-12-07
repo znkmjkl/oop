0c244bdbe57ff747124ff4dbfbbbfc456960a6b8
Michał P <p.michal1990@gmail.com>
2014-12-07 15:48:03 +0100

    poprawienie testow

--- shapes/src/test/java/com/shapes/implementations/DifferenceTest.java
+++ shapes/src/test/java/com/shapes/implementations/DifferenceTest.java
@@ -16,21 +16,24 @@ public class DifferenceTest {
 		Rectangle shape1 = new Rectangle(10, 16);
 		Ellipse shape2 = new Ellipse(6, 10);
 		Rectangle shape3 = new Rectangle(6,8);
+		
 		Difference d = new Difference(shape1, shape2);
 		
-		assertTrue(d.contains(5, 5));
+		assertTrue(d.contains(5, 6));
+		assertTrue(d.contains(5, 8));
 		
 		assertFalse(d.contains(0, 0));
 		assertFalse(d.contains(2, 2));
 		assertFalse(d.contains(1, 2));
 		assertFalse(d.contains(2, 1));	
-		assertFalse(d.contains(5, 8));
+		
 		
 		Difference d1 = new Difference(shape1, shape3);
 		assertFalse(d1.contains(0, 0));
-		assertFalse(d1.contains(5, 8));	
+		assertFalse(d1.contains(8, 5));
 		
-		assertTrue(d1.contains(8, 5));
+		assertTrue(d1.contains(5, 8));		
+		assertTrue(d1.contains(4, 4.5f));
 			
 	}
 	
